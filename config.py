54f6f703e8f6030afb4d88c0038dbecfa24f3c9b
Remax-Top <104597232+topaxn@users.noreply.github.com>
2022-04-29 01:40:39 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ from pytgcalls import PyTgCalls
 if os.path.exists(".env"):
     load_dotenv(".env")
 # الفارات
-API_ID = int(os.getenv("ايبي ايدي"))
-API_HASH = os.getenv("ايبي هاش")
-SESSION = os.getenv("بايرو توب")
-HNDLR = os.getenv("علامه او رمز", "$")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
+SESSION = os.getenv("SESSION")
+HNDLR = os.getenv("HNDLR", "$")
 SUDO_USERS = list(map(int, os.getenv("SUDO_USERS").split()))
 contact_filter = filters.create(    lambda _, __, message: (message.from_user and message.from_user.is_contact)    or message.outgoing)
 bot = Client(SESSION, API_ID, API_HASH, plugins=dict(root="MusicTop"))
