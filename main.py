93204a1f249a29665844b3b8b924a6b7a5819371
Remax-Top <104597232+topaxn@users.noreply.github.com>
2022-04-29 01:17:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+from pytgcalls import idle
+from config import call_py
+from MusicTelethon.التشغيل import arq
+async def main():
+    await call_py.start()
+    print("""    ------------------
+   | ميوزك تليثون الان شغال ! |
+    ------------------"""    )
+    print("""
+🟥🟥🟥🟥🟥🟥
+☆ الله ☆ اكبر☆
+⬛️⬛️⬛️⬛️⬛️⬛️     """)
+    await idle()
+    await arq.close()
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
